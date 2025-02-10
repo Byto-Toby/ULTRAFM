8f331e8b1c18899f860556dab76511785856dd98
Francis Pereira Leite <francisplj080@gmail.com>
2025-02-10 16:02:30 -0300

    Json server,script and cards

---
+++ script.js
@@ -0,0 +1,10 @@
+const searchInput = document.getElementById('search-input');
+const resultsArtists = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlists');
+
+document.addEventListener('input', () => {
+    const searchTerm = searchInput.value.toLowecase();
+    if (searchTerm === '') {
+        
+    }
+})
